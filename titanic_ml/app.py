e80f2e03dad12d79ea6210d888e0067e15d56ea0
Khushwant Singh <khuswhant2001@gmail.com>
2025-12-16 16:01:35 +0100

    added validation

--- titanic_ml/app.py
+++ titanic_ml/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import joblib
 import pandas as pd
 
@@ -8,14 +8,21 @@ app = FastAPI(title = "Titanic Survival Prediction API ")
 model = joblib.load("model/titanic_model.pkl")
 features = joblib.load("model/titanic_features.pkl") 
 
-class Passenger(BaseModel):
-    Pclass: int
-    Sex: int
-    Age: float
-    Fare: float
-    Embarked: int
-    FamilySize: int
+# class Passenger(BaseModel):
+#     Pclass: int
+#     Sex: int
+#     Age: float
+#     Fare: float
+#     Embarked: int
+#     FamilySize: int
 
+class Passenger(BaseModel):
+    Pclass: int = Field(..., ge=1, le=3, description="Passenger class (1, 2, or 3)")
+    Sex: int = Field(..., ge=0, le=1, description="0 = male, 1 = female")
+    Age: float = Field(..., gt=0, le=100, description="Age in years")
+    Fare: float = Field(..., ge=0, le=600, description="Ticket fare")
+    Embarked: int = Field(..., ge=0, le=2, description="0=S, 1=C, 2=Q")
+    FamilySize: int = Field(..., ge=1, le=11, description="Family size (1–11)")
 
 @app.post("/predict")
 def predict_survival(passenger: Passenger):
